22f73c659604c7c1d7650182038b1176f0b2dea5
Maynard <me@maynards.site>
2022-11-27 11:35:20 -0800

    move cpucontrol and ipv6-disable from systems/<system>.nix into common.nix

--- systems/lake.nix
+++ systems/lake.nix
@@ -51,8 +51,6 @@ boot = {
   boot.zfs.devNodes = "/dev/disk/by-path";
   networking.hostId = "00c06c06";  # required by zfs
 
-  # WANT: ability to disable eyd for single reboot
-  #       wasn't able to figure out a good way to do this tho
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     zfs rollback -r rpool/eyd/root@blank
   '';
@@ -63,64 +61,6 @@ base = {
   networking.hostName = "lake";
   system.stateVersion = "22.05";
   time.timeZone = "America/Los_Angeles";
-
-  # Disable ipv6; it's messing with npm n stuff
-  boot.kernel.sysctl."net.ipv6.conf.eth0.disable_ipv6" = true;
-};
-
-# =============================================================================
-cpu-control = let
-
-  scriptname = "set-cpu-governors";
-
-in {
-
-  security.wrappers.${scriptname} = {
-    source = pkgs.writeScript scriptname ''
-      #!${pkgs.bash}/bin/bash
-      gov="$1"
-      for i in {0..7}; do
-        sudo cpufreq-set -g "$gov" -c $i
-      done
-    '';
-    setuid = true;  # nb Set +s so it can be run from ulauncher
-    owner = "root";
-    group = "root";
-  };
-
-  environment.systemPackages = [
-
-    pkgs.cpufrequtils
-
-    (pkgs.writeScriptBin "perf.fast" ''
-      ${scriptname} performance "$@"
-    '')
-
-    (pkgs.writeScriptBin "perf.slow" ''
-      ${scriptname} powersave "$@"
-    '')
-
-    (pkgs.writeScriptBin "perf.which" ''
-      # assumes all CPUs are set to the same governor
-      mode=$(cpufreq-info | grep 'The governor' | awk -F\" '{print $2}' | head -n1)
-
-      case "$mode" in
-        performance) echo -n fast ;;
-        powersave) echo -n slow ;;
-        *) echo -n '???' ;;
-      esac
-    '')
-
-    (pkgs.writeScriptBin "perf.switch" ''
-      case "$(perf.which)" in
-        fast) perf.slow ;;
-        slow) perf.fast ;;
-        *) perf.fast ;;
-      esac
-    '')
-
-  ];
-
 };
 
 # =============================================================================
