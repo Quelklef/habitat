da95073e714fd2afccbe27de98a3ce40298a5143
Maynard <me@maynards.site>
2022-11-27 09:20:11 -0800

    support deletion of multiple files with 'del'

--- files/scripts/del.sh
+++ files/scripts/del.sh
@@ -8,26 +8,31 @@ set -euo pipefail
 
 trash_loc=$TRASH_LOC
 
-[ "$#" = 1 ] || { echo >&2 "Expecting exactly 1 argument"; exit 1; }
-src=$(realpath "$1")
+function del_one {
+  src=$1
 
-[ -e "$src" ] || { echo >&2 "Nothing at $src"; exit 1; }
+  [ -e "$src" ] || { echo >&2 "Nothing at $src"; exit 1; }
 
-src_name=$(basename "$src")
-dest=$trash_loc/$(date +%s.%N-%Y-%m-%d_%H-%M-%S)-$src_name
+  src_name=$(basename "$(realpath "$src")")
+  dest=$trash_loc/$(date +%s.%N-%Y-%m-%d_%H-%M-%S)-$src_name
 
-mkdir -p "$trash_loc"
+  mkdir -p "$trash_loc"
 
-cp -r $src $dest
-[ $? = 0 ] || {
-  echo >&2 "Failed to copy $src"
-  rm -rf $dest
-  exit 1
+  cp -r -- $src $dest
+  [ $? = 0 ] || {
+    echo >&2 "Failed to copy $src"
+    rm -rf -- $dest
+    exit 1
+  }
+
+  rm -rf $src
+  [ $? = 0 ] || {
+    echo >&2 "Failed to delete $src. Partial deletion may have ocurred"
+    echo >&2 "A copy of $src was made at $dest"
+    exit 1
+  }
 }
 
-rm -rf $src
-[ $? = 0 ] || {
-  echo >&2 "Failed to delete $src. Partial deletion may have ocurred"
-  echo >&2 "A copy of $src was made at $dest"
-  exit 1
-}
+for arg in "$@"; do
+  del_one "$arg"
+done
