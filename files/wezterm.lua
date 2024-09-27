d42b0bd7d0ba0048b4cd7518e87256b7664e54d1
Maynard <me@maynards.site>
2024-09-26 18:15:05 -0700

    wezterm tweaks

--- files/wezterm.lua
+++ files/wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require 'wezterm'
 
 
--- Lib --
+--|| Lib ||--
 
 function trim(str)
   return str:match( "^%s*(.-)%s*$" )
 end
 
 
--- Config --
+--|| Config ||--
 
 local config = wezterm.config_builder()
 
@@ -24,11 +24,25 @@ else
   config.color_scheme = default_color_scheme
 end
 
+
 config.font = wezterm.font 'monospace'
 config.font_size = 9
+
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = { left = 2, right = 2, top = 2, bottom = 2 }
 config.window_background_opacity = 0.97
+
+
+-- Do not ask "Really kill this window [...]?"
+config.window_close_confirmation = "NeverPrompt"
+
+
+-- I don't know, in detail, what this setting does.
+--
+-- However, without it, Kak does not seem to receive shift+space keypresses: the input
+-- sequence '<shift down>ab cd<shift up>' produces 'ABCD' instead of 'AB CD'. Super annoying!
+config.enable_csi_u_key_encoding = true
+
 
 return config
